b1105c1817ca58e5aec0289157de71952b9647a4
GENERALRSW <raheemw018@gmail.com>
2026-03-17 00:50:22 -0500

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
-# import flask migrate here
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-# Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
-# Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+from app import models
+from app.models import UserProfile
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(UserProfile, int(user_id))
 
 from app import views
